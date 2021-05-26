515b5414f60b75899582a4c7ccf0893d9a1ad700
Shreevatsa MS <shreevatsa.dush@gmail.com>
2021-05-26 07:50:58 +0530

    workflow test

---
+++ core/src/main/java/com/mindtree/c1/core/service/WedTestComponent.java
@@ -0,0 +1,46 @@
+package com.mindtree.c1.core.service;
+
+import javax.jcr.Node;
+import javax.jcr.Session;
+
+import org.osgi.framework.Constants;
+import org.osgi.service.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.adobe.granite.workflow.WorkflowException;
+import com.adobe.granite.workflow.WorkflowSession;
+import com.adobe.granite.workflow.exec.WorkItem;
+import com.adobe.granite.workflow.exec.WorkflowData;
+import com.adobe.granite.workflow.exec.WorkflowProcess;
+import com.adobe.granite.workflow.metadata.MetaDataMap;
+
+@Component(service = WorkflowProcess.class, property = { "process.label" + " = WedTestComponent",
+		Constants.SERVICE_VENDOR + "=WedTestComponent", Constants.SERVICE_DESCRIPTION + " = WedTestComponent" })
+public class WedTestComponent implements WorkflowProcess {
+	private static final Logger LOG = LoggerFactory.getLogger(WedTestComponent.class);
+
+	@Override
+	public void execute(WorkItem workItem, WorkflowSession workflowSession, MetaDataMap processArguments)
+			throws WorkflowException {
+		// TODO Auto-generated method stub
+		try {
+			WorkflowData workflowData = workItem.getWorkflowData();
+			if (workflowData.getPayloadType().equals("JCR_PATH")) {
+				Session session = workflowSession.adaptTo(Session.class);
+				String path = workflowData.getPayload().toString() + "/jcr:content";
+				Node node = (Node) session.getItem(path);
+				String brand = processArguments.get("BRAND", "");
+				boolean multinational = processArguments.get("MULTINATIONAL", false);
+				LOG.info("\n BRAND : {} , MULTINATIONAL : {} ", brand, multinational);
+				String[] countries = processArguments.get("COUNTRIES", new String[] {});
+				for (String country : countries) {
+					LOG.info("\n Countries {} ", country);
+				}
+			}
+		} catch (Exception e) {
+			LOG.info("\n ERROR {} ", e.getMessage());
+		}
+	}
+
+}
